1d7dee4af221fbc5958b182b29ff4b452043ce2e
Avishai Micaiah <avishai@macbook-pro.mynetworksettings.com>
2025-07-24 17:27:23 -0700

    Allow iframe embedding for /embed/* routes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   // You can add other Next.js config options here if needed
+  async headers() {
+    return [
+      {
+        source: '/embed/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
